a7f689a9f4a39aad7f757eab5b6efba5befc4a60
Shubhi Gupta <shubhi.99gupta@gmail.com>
2024-10-19 18:42:30 -0500

    Completed: API_Users and API_assets

--- project02-server-main/api_users.js
+++ project02-server-main/api_users.js
@@ -5,25 +5,73 @@
 //
 const photoapp_db = require('./photoapp_db.js')
 
+
+//
+// query_database:
+//
+// Queries database, returning a PROMISE that you can
+// await on. When the PROMISE resolves, you'll have the 
+// results of your query (or you'll get an error thrown
+// back).
+//
+function query_database(db, sql)
+{
+  let response = new Promise((resolve, reject) => {
+    try 
+    {
+      //
+      // execute the query, and when we get the callback from
+      // the database server, either resolve with the results
+      // or error with the error object
+      //
+      db.query(sql, (err, results, _) => {
+        if (err) {
+          reject(err);
+        }
+        else {
+          resolve(results);
+        }
+      });
+    }
+    catch (err) {
+      reject(err);
+    }
+  });
+  
+  // 
+  // return the PROMISE back to the caller, which will
+  // eventually resolve to results or an error:
+  //
+  return response;
+}
+
+
+//
+// get /users:
+//
 exports.get_users = async (req, res) => {
 
   console.log("**Call to get /users...");
 
   try {
 
+    //
+    // query to select all users from user table in ascending order by user id
+    //
+    let sql = `SELECT * FROM users ORDER BY userid ASC;`;
+    console.log("/users: calling RDS to get user table...");
+    let mysql_promise = query_database(photoapp_db, sql);
+    let results = await Promise.all([mysql_promise]);
+    let rds_results = results[0];
     
-    throw new Error("TODO: /users");
-
     //
-    // TODO: remember we did an example similar to this in class with
-    // movielens database
+    // done, respond with users table:
     //
-    // MySQL in JS:
-    //   https://expressjs.com/en/guide/database-integration.html#mysql
-    //   https://github.com/mysqljs/mysql
-    //
-    
-
+    console.log("/users done, sending response...");
+    res.json({
+      "message": "success",
+      "data": rds_results
+    });
   }//try
   catch (err) {
     console.log("**Error in /users");
